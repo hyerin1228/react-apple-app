0bc862c9a8acd9312078fd1b94b5051e42d84231
hyerin1228 <hyerin1228@gmail.com>
2024-01-29 10:31:15 +0900

    #1. The Movie DB API 요청을 위한 Axios 인스턴스 생성

---
+++ src/api/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios"
+
+// eslint-disable-next-line no-unused-vars
+const instance = axios.create({
+    baseURL: "https://api.themoviedb.org/3",
+    params: {
+        api_key: "496f7be28f97f1e3ebb1e2f650bc100c",
+        langueage: "ko-KR",
+
+    },
+});
