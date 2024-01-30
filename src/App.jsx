cb6704ab8b1eb222fddfa455b8b29898bc0d3c43
hyerin1228 <hyerin1228@gmail.com>
2024-01-30 11:20:16 +0900

    #6. Row 컴포넌트 생성

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { styled } from "styled-components"
 import './App.css'
 import Nav from './components/MovieModal/Nav'
 import Banner from "./components/MovieModal/Banner"
+import requests from "./api/request";
+import Row from "./components/MovieModal/Row"
 
 function App() {
 
@@ -9,6 +11,10 @@ function App() {
     <Container>
       <Nav />
       <Banner />
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies} />
     </Container>
   )
 }
--- src/components/MovieModal/Row.jsx
+++ src/components/MovieModal/Row.jsx
@@ -1,8 +1,46 @@
+/* eslint-disable react/prop-types */
+import axios from "../../api/axios"
 import './Row.css'
+import { useCallback, useEffect, useState } from "react"
 
-const Row = () => {
+const Row = ({ title, id, fetchUrl }) => {
+
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovieData = useCallback(async () => {
+    const response = await axios.get(fetchUrl);
+    setMovies(response.data.results);
+  }, [fetchUrl])
+  
+  useEffect(() => {
+    fetchMovieData();
+  }, [fetchMovieData])
+  
   return (
-    <div>Row</div>
+    <div>
+      <h2>{title}</h2>
+      <div className="slider">
+        <div className="slider__arrow-left">
+          <span className="arrow">
+            {"<"}
+          </span>
+        </div>
+        <div id={id} className="row__posters">
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              className="row__poster"
+              src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+            />
+          ))}
+        </div>
+        <div className="slider__arrow-right">
+          <span className="arrow">
+            {">"}
+          </span>
+        </div>
+      </div>
+    </div>
   )
 }
 
