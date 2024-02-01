2a3d27fb9f394a0e38b1f2c27ada1519fb00192b
hyerin1228 <hyerin1228@gmail.com>
2024-02-02 00:38:06 +0900

    #13. localStorage에 로그인에 따른 user 데이터 저장 및 삭제

--- src/components/MovieModal/Nav.jsx
+++ src/components/MovieModal/Nav.jsx
@@ -1,14 +1,17 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useLocation } from 'react-router-dom';
 import { styled } from "styled-components";
-import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from "firebase/auth";
 import app from '../../firebase';
 
+const initialUserData = localStorage.getItem('userData') ? JSON.parse(localStorage.getItem('userData')) : {} ;
 
 const Nav = () => {
     
     const [show, setShow] = useState("false");
 
+  
+    const [userData, setUserData] = useState(initialUserData);
     const [searchValue, setSearchValue] = useState('');
     const navigate = useNavigate();
 
@@ -55,9 +58,20 @@ const Nav = () => {
       signInWithPopup(auth, provider)
       // eslint-disable-next-line no-unused-vars
       .then((result) => {
-
+        setUserData(result.user);
+        localStorage.setItem('userData', JSON.stringify(result.user));
       })
       .catch((error) => {
+        alert(error.message);
+      })
+    }
+
+    // 로그아웃
+    const handleLogOut = () => {
+      signOut(auth).then(() => {
+        setUserData({});
+        localStorage.removeItem('userData')
+      }).catch((error) => {
         alert(error.message);
       })
     }
@@ -86,9 +100,64 @@ const Nav = () => {
           />
         )
       }
+
+      {pathname !== "/" ?
+        <SignOut>
+          <UserImg src={userData.photoURL} alt={userData.displayName} />
+          <DropDown>
+            <span onClick={handleLogOut}>
+              Sign Out
+            </span>
+          </DropDown>
+        </SignOut>
+
+        : null
+      }
+
+
+
     </NavWrapper>
   )
 }
+
+const DropDown = styled.div`
+  position: absolute;
+  top: 48px; 
+  right: 0px;
+  background: rgb (19, 19, 19);
+  border: 1px solid rgba (151, 151, 151, 0.34); 
+  border-radius: 4px;
+  box-shadow: rgb(0 0 0 / 50%) 0px 0px 18px 0рх; 
+  padding: 10px; 
+  font-size: 14px; 
+  letter-spacing: 3px; 
+  width: 100px; 
+  opacity: 0;
+`;
+
+const SignOut = styled.div`
+  position: relative;
+  height: 48px; 
+  width: 48px; 
+  display: flex; 
+  cursor: pointer; 
+  align-items: center; 
+  justify-content: center;
+
+  &:hover {
+    ${DropDown}{
+      opacity: 1;
+      transition-duration: 1s;
+    }
+  }
+`;
+
+
+const UserImg = styled.img`
+  border-radius: 50%; 
+  width: 100%; 
+  height: 100%;
+`;
 
 
 const Input = styled.input`
